a951d69378e22bfe343985bc3c12b7deca8d2628
TranTheSon2206 <109707502+TranTheSon2206@users.noreply.github.com>
2022-10-28 14:36:24 +0700

    1-3

---
+++ nameController.js
@@ -0,0 +1,18 @@
+angular.module('myApp',[]).controller('namesCtrl',function($scope){
+    $scope.names = [
+        {country:'Brazil',capital:'Brasilia'},
+        {country:'Russia',capital:'Moscow'},
+        {country:'China',capital:'Beijing'},
+        {country:'Canada',capital:'Ottawa'},
+        {country:'USA',capital:'Washington D.C'},
+        {country:'Korea',capital:'Seoul'},
+        {country:'United Kingdom',capital:'London'},
+        {country:'Italia',capital:'Roma'},
+        {country:'France',capital:'Paris'},
+        {country:'Germany',capital:'Berlin'},
+        {country:'Australia',capital:'Canberra'},
+        {country:'Venezuela',capital:'Caracas'},
+        {country:'Spain',capital:'Madrid'},
+        {country:'VietNam',capital:'HaNoi'},
+    ];
+});
